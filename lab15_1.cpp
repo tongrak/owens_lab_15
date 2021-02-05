5869514870b6c66686fea9978f2adc86b20a8d33
Tongrak Ruento <tongrak_ru@cmu.ac.th>
2021-02-05 16:13:13 +0700

    Lab15 done!

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -32,3 +32,46 @@ int main(){
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
 }
+
+void showData(double *A, int N ,int M){
+	cout << fixed << setprecision(2);
+    for(int i = 0; i < N*M; i++){
+        cout << *(A+i);
+        if((i+1)%M==0) cout << endl;
+        else cout << " ";
+    }
+}
+
+void randData(double *A, int N, int M){
+	for(int i=0; i<N*M;i++){
+		*(A+i)=(rand()%101)/100.0;
+	}
+}
+
+void findRowSum(const double *A, double *out, int Y, int X){
+	int v=0;
+	double holder=0;
+	for(int i=0;i<X*Y;i++){
+		holder+=*(A+i);
+		if((i+1)%X==0){
+			*(out+v)=holder;
+			holder=0;
+			v++;
+		}
+	}
+}
+
+void findColSum(const double *A, double *out, int Y, int X){
+	int v=0;
+	double holder[X]={};
+	for(int i=0;i<X*Y;i++){
+		holder[v]+=*(A+i);
+		v++;
+		if((i+1)%X==0){
+			v=0;
+		}
+	}
+	for(int j=0;j<X;j++){
+		*(out+j)=holder[j];
+	}
+}
